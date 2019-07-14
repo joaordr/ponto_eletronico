af20c1be2371326adb7de1997532bd9d5669455d
JulianeSBernardino <52682074+JulianeSBernardino@users.noreply.github.com>
2019-07-14 14:44:45 -0300

    update

--- view/cadastroFuncionario.php
+++ view/cadastroFuncionario.php
@@ -1,42 +1,54 @@
-<div class="panel panel-default">
+<script type="text/javascript" src="../resources/js_page/cadastroFuncionario.js"></script>
+<div class="row">
+   <div class="col"></div>
+   <div class="col-lg-6">
+      <div class="panel panel-default jumbotron">
   <div class="panel-body">
-   	<form name="cadFuncionario" id="cadFuncionario" method="POST">
-   		<div class="form-group">
-   			<label for="nome">Nome completo</label>
-   			<input type="text" name="nome" id="nome" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="cpf">CPF</label>
-   			<input type="text" name="cpf" id="cpf" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="rg">RG</label>
-   			<input type="text" name="rg" id="rg" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="dtNasc">Data de Nascimento</label>
-   			<input type="date" name="dtNasc" id="dtNasc" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="email">Email</label>
-   			<input type="email" name="email" id="email" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="telefone">Telefone</label>
-   			<input type="text" name="telefone" id="telefone" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="cargo">Cargo</label>
-   			<input type="text" name="cargo" id="cargo" class="form-control">
-   		</div>
-   		<div class="form-group">
-   			<label for="setor">Setor</label>
-   			<input type="text" name="setor" id="setor" class="form-control">
-   		</div>
-   		<button type="submit" class="btn btn-primary">Cadastrar</button>
-   	</form>
+      <form name="cadFuncionario" id="cadFuncionario" method="POST">
+         <div class="form-group">
+            <label for="nome">Nome completo</label>
+            <input type="text" name="nome" id="nome" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="cpf">CPF</label>
+            <input type="text" name="cpf" id="cpf" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="rg">RG</label>
+            <input type="text" name="rg" id="rg" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="dtNasc">Data de Nascimento</label>
+            <input type="date" name="dtNasc" id="dtNasc" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="telefone">Telefone</label>
+            <input type="text" name="telefone" id="telefone" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="cargo">Cargo</label>
+            <input type="text" name="cargo" id="cargo" class="form-control">
+         </div>
+         <div class="form-group">
+            <label for="setor">Setor</label>
+            <input type="text" name="setor" id="setor" class="form-control">
+         </div>
+         <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </form>
   </div>
 </div>
-<script type="text/javascript" src="../resources/js_page/cadastroFuncionario.js"></script>
+   </div>
+   <div class="col"></div>
+</div>
 
-
+<style type="text/css">
+   .panel{
+      border-radius: 20px;
+      margin-top: 20px;
+      border: 1px solid #D3D3D3;
+   }
+</style>
