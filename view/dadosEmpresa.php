f45989a3220a9bad5432f528e97553a1c3b042e7
João Rodrigo Prieto <jrp2033@gmail.com>
2019-07-14 16:41:35 -0300

    update

--- view/dadosEmpresa.php
+++ view/dadosEmpresa.php
@@ -1,35 +1,39 @@
 <div class="row">
-  <div class="col"></div>
-  <div class="col">
-    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalCadEmpresa">Alterar</button>
-  </div>
-  <div class="col"></div>
+    <div class="col"></div>
+    <div class="col">
+        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalCadEmpresa">Alterar
+        </button>
+    </div>
+    <div class="col"></div>
 </div>
 <div class="row">
-  <div class="col"></div>
-  <div class="col">
-      <div class="panel panel-default jumbotron">
-        <div class="panel-body">
-            <h4 id="nomeEmpresa">Nome da Empresa</h4>
-            <h5 id="cnpj">41.168.726/0001-15</h5>
+    <div class="col"></div>
+    <div class="col">
+        <div class="panel panel-default jumbotron">
+            <div class="panel-body">
+                <h4 id="nomeEmpresa">Nome da Empresa</h4>
+                <h5 id="cnpj">41.168.726/0001-15</h5>
+            </div>
         </div>
-      </div>
-  </div>
-  <div class="col"></div>
+    </div>
+    <div class="col"></div>
 </div>
 
-  <style type="text/css">
-     .panel{
-      margin-top: 15px;
-      border: 1px solid #D3D3D3;
-      width: 500px
-     }
-     button{
-      margin-top: 20px;
-      margin-left: 130px;
-     }
-     h4, h5{
-      text-align: justify;
-     }
-  </style>
-</div>
+<style type="text/css">
+    .panel {
+        margin-top: 15px;
+        border: 1px solid #D3D3D3;
+        width: 500px
+    }
+
+    button {
+        margin-top: 20px;
+        margin-left: 130px;
+    }
+
+    h4, h5 {
+        text-align: justify;
+    }
+</style>
+
+<script type="text/javascript" src="../resources/js_page/dadosEmpresa.js"></script>
