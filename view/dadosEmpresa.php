a1535a62d7134d318fb6b50e5d5f86ce954ce2ae
JulianeSBernardino <52682074+JulianeSBernardino@users.noreply.github.com>
2019-07-14 16:43:43 -0300

    update

--- view/dadosEmpresa.php
+++ view/dadosEmpresa.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col"></div>
   <div class="col">
-    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalCadEmpresa">Alterar</button>
+    <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalUpdateEmpresa">Alterar</button>
   </div>
   <div class="col"></div>
 </div>
@@ -16,6 +16,43 @@
       </div>
   </div>
   <div class="col"></div>
+</div>
+
+<!-- Modal update empresa -->
+<div class="modal fade" id="modalUpdateEmpresa" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Alterar empresa</h5>
+            </div>
+            <div class="modal-body">
+                <form name="cadEmpresa" id="updateEmpresa" method="POST">
+                        <div class="form-group">
+                            <label for="nome">Nome da empresa</label>
+                            <input type="text" name="nome" id="nome" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="razaoSocial">Razão Social</label>
+                            <input type="text" name="razaoSocial" id="razaoSocial" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="cnpj">CNPJ</label>
+                            <input type="text" name="cnpj" id="cnpj" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="ramo">Ramo de atividade</label>
+                            <input type="text" name="ramo" id="ramo" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="telefone">Telefone</label>
+                            <input type="text" name="telefone" id="telefone" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Cadastrar</button>
+                    </form>
+            </div>
+        </div>
+    </div>
 </div>
 
   <style type="text/css">
