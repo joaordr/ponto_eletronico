4d0bc21468e78e3b66c4ddaed676dcf1a037a6cc
João Rodrigo Prieto <jrp2033@gmail.com>
2019-07-14 18:05:44 -0300

    update

--- resources/js_page/cadastroFuncionario.js
+++ resources/js_page/cadastroFuncionario.js
@@ -1,4 +1,49 @@
 $(document).ready(function () {
+    $("#cadFuncionario1").validate({
+        rules:{
+            nome: {
+                required: true,
+                maxlength: 100
+            },
+            cpf: {
+                cpfBR: true,
+                required: true
+            },
+            rg:{
+                required: true,
+            },
+            dtNasc:{
+                required: true,
+                date: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            telefone:{
+                required: true,
+                maxlength: 10
+            },
+            cargo:{
+                required: true,
+                maxlength: 100
+            },
+            setor:{
+                required: true,
+                maxlength: 100
+            },
+            nomeUsuario:{
+                required: true,
+                maxlength: 100
+            },
+            senhaFunc:{
+                required: true,
+                maxlength: 10
+
+            }
+        }
+    });
+
     $("#cadFuncionario1").submit(function (e) {
         let dados = $(this).serialize() + "&create_func=true";
         let retorno = request("funcionario", dados);
